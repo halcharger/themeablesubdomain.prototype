d7ad02869c1ad90c06ae24a534274ce8d9ee5cbf
halcharger <allen.firth@gmail.com>
2014-11-03 09:41:25 +0200

    adding initial website

---
+++ ThemeableSubdomains.Prototype/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ThemeableSubdomains.Prototype.Startup))]
+namespace ThemeableSubdomains.Prototype
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
